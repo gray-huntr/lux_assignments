d7adfef29284d65f18c700d369b7671f7d56f895
Lennox <lkulecho2001@gmail.com>
2025-07-06 19:28:02 +0300

    variables assignment added

---
+++ python_basics/variables.py
@@ -0,0 +1,25 @@
+# Assigning my age to the variable age
+age = 24
+# Declaring 2 variables x and y
+x = 10
+y = 5
+# doing the sum of the 2 variables
+data_sum = x + y
+# printing the output of the sum
+print(data_sum)
+# output = 15
+# declaring variable with invalid variable name
+# 4ndname = 4
+# output = SyntaxError: invalid decimal literal
+
+# Assigning a string to a variable
+name = "Lennox"
+print(name)
+# output = Lennox
+# Dynamically assigning variables from the user using the 'input' function
+challenge_name = input("What is your name? ") # name = Lennox
+challenge_age = input("What is your age? ") # age = 23
+print(f"Hello {challenge_name}. \nYou are {challenge_age} years old.")
+# output = "Hello Lennox.
+# You are 23 years old."
+
